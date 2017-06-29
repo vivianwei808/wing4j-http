2acbf730daa81510f1b5d224b761b42d769d4e51
wing4j <wing4j@foxmail.com>
2017-06-29 22:34:18 +0800

    xiugai

--- wing4j-http-server/wing4j-http-server-springmvc/src/main/java/org/wing4j/http/web/controller/ApiController.java
+++ wing4j-http-server/wing4j-http-server-springmvc/src/main/java/org/wing4j/http/web/controller/ApiController.java
@@ -2,12 +2,10 @@ package org.wing4j.http.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.wing4j.http.server.InterfaceAccessEngine;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
 /**
  * Created by wing4j on 2017/6/27.
@@ -18,10 +16,7 @@ public class ApiController {
     InterfaceAccessEngine interfaceAccessEngine;
 
     @RequestMapping("/api")
-    public String api(@RequestBody String body, String encode) throws UnsupportedEncodingException {
-        if("true".equals(encode)){
-            body = URLEncoder.encode(body, "UTF-8");
-        }
+    public String api(String body) throws UnsupportedEncodingException {
         return interfaceAccessEngine.execute(body);
     }
 }
